efe4ad5fc7cf8d42bdd41d4d25e15e530bdf1773
codewalker <codewalker@hardkernel.com>
2014-05-31 17:07:46 +0900

    4412 4.1.2 master

--- bluedroid/Android.mk
+++ bluedroid/Android.mk
@@ -6,12 +6,26 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(BOARD_HAVE_BLUETOOTH_BCM),true)
+LOCAL_SRC_FILES := \
+	bluetooth_bcm4329.c
+else
+ifeq ($(BOARD_HAVE_BLUETOOTH_USB),true)
+LOCAL_SRC_FILES := \
+	bluetooth_usb.c
+else
 LOCAL_SRC_FILES := \
 	bluetooth.c
+endif
+endif
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include \
 	system/bluetooth/bluez-clean-headers
+
+ifeq ($(BOARD_BLUETOOTH_BCM4329), true)
+LOCAL_CFLAGS := -DBCM4329_MODULE
+endif
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils
